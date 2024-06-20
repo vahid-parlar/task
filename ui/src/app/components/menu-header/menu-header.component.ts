efd25d17cb93599eb99ee75b6de052ff59465383
jalal sagaie <jalal.sagaie@gmail.com>
2024-06-20 20:35:14 +0330

    add-exit

--- ui/src/app/components/menu-header/menu-header.component.ts
+++ ui/src/app/components/menu-header/menu-header.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { MaterialModule } from '@modules/Material.module';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { LocalStorageService } from '@services/common/local-storage.service';
 
 @Component({
   selector: 'app-menu-header',
   standalone: true,
   imports: [RouterLink, MaterialModule],
   templateUrl: './menu-header.component.html',
-  styleUrl: './menu-header.component.scss'
+  styleUrl: './menu-header.component.scss',
 })
 export class MenuHeaderComponent {
-
+  constructor(
+    private localStorageService: LocalStorageService,
+    private router: Router
+  ) {}
+  onLogOut() {
+    this.localStorageService.removeLoginResult();
+    this.router.navigate(['/login']);
+  }
 }
